94e7fd635f51f4f9a0525a8816bfd4483005b6ca
Field User <field@ccom.unh.edu>
2025-03-11 18:56:08 +0000

    Add lifecycle launch file

---
+++ launch/echo_helm_launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import PythonExpression
+from launch_ros.actions import LifecycleNode
+from launch_ros.actions import LifecycleTransition
+from launch_ros.actions import Node
+
+from lifecycle_msgs.msg import Transition
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        LifecycleNode(
+            package='echo_helm',
+            executable='echo_helm_node',
+            name='echo_helm',
+            namespace='',
+            respawn=True,
+            respawn_delay=2,
+        ),
+        LifecycleTransition(
+            lifecycle_node_names=(
+                PythonExpression(
+                    expression = [
+                        '"',
+                        LaunchConfiguration("ros_namespace", default=''),
+                        '" + "/echo_helm"'
+                    ],
+                ),
+            ),
+            transition_ids=(
+                Transition.TRANSITION_CONFIGURE,
+                Transition.TRANSITION_ACTIVATE,
+            )
+        ),
+    ])
